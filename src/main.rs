0043cf32f0649587f63ac5b4032dd8a1ad7c3558
Conzel <38732545+Conzel@users.noreply.github.com>
2021-03-09 12:01:54 +0100

    FIX: Simple camera errors
    PPM files were written in the wrong order (horiz/vert)

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,29 @@ const FOCAL_LENGTH: f64 = 1.0;
 const ORIGIN: vec3::Loc = vec3::Vec3::new(0.0,0.0,0.0);
 const HORIZ: vec3::Loc = vec3::Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
 const VERT: vec3::Loc = vec3::Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
-const LOWER_LEFT: vec3::Loc = vec3::Vec3::new(-VIEWPORT_HEIGHT, -VIEWPORT_WIDTH, -FOCAL_LENGTH);
 
 fn ray_to_color(r: &ray::Ray) -> vec3::Color {
     use vec3::*;
     let dir = r.unit_direction();
     let t = 0.5 * (dir.e1 + 1.0);
-    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5,0.7,1.0)
+    assert!(t >= 0.0 && t <= 1.0);
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.2,0.4,1.0)
 }
 
 fn main() {
     use vec3::*;
     use ray::*;
 
-    let bar = ProgressBar::new((IMAGE_WIDTH).into());
+    let lower_left: vec3::Loc = ORIGIN - HORIZ / 2.0 - VERT / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+
+    let bar = ProgressBar::new((IMAGE_HEIGHT).into());
     println!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
-    for row in 0..IMAGE_WIDTH {
+    for col in 0..IMAGE_HEIGHT {
         bar.inc(1);
-        for col in 0..IMAGE_HEIGHT {
+        for row in 0..IMAGE_WIDTH {
             let u = (row as f64) / ((IMAGE_WIDTH - 1) as f64);
             let v = (col as f64) / ((IMAGE_HEIGHT - 1) as f64);
-            let r = Ray::new(ORIGIN, &HORIZ * u + &VERT * v + LOWER_LEFT - ORIGIN);
+            let r = Ray::new(ORIGIN, &HORIZ * u + &VERT * v + &lower_left - ORIGIN);
 
             let c: Color = ray_to_color(&r);
 
